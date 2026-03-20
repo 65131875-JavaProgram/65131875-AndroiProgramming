85c5af121fac501fff837945dc6fd26fb5d7ed7f
65131875-Nguyễn thị yến ly <ly.nty.65cntt@ntu.edu.vn>
2026-03-20 14:05:30 +0700

    TH6
    TH6

---
+++ Xulisukien_Tong2so/app/src/main/java/tiil/edu/xulisukien_tong2so/MainActivity.java
@@ -0,0 +1,42 @@
+package tiil.edu.xulisukien_tong2so;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    //Đây là bộ lắng nghe và xử lí sự kiện click lên nút tính tổng
+    public void XuLyCong(View view){
+      EditText ediTextSoA =  findViewById(R.id.edtA);
+      EditText ediTextSoB =  findViewById(R.id.edtB);
+      EditText ediTextKetqua =  findViewById(R.id.edtKQ);
+
+     String strA = ediTextSoA.getText().toString();
+     String strB = ediTextSoB.getText().toString();
+
+     int so_A =   Integer.parseInt(strA);
+     int so_B =   Integer.parseInt(strB);
+
+     int tong = so_A + so_B;
+     String strTong = String.valueOf(tong);
+     ediTextKetqua.setText(strTong);
+    }
+}
