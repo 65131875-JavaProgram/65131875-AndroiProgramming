af3e5fe03a4d81d3005a4c4570d5ddfcf6761566
65131875-Nguyễn thị yến ly <ly.nty.65cntt@ntu.edu.vn>
2026-04-02 21:26:41 +0700

    TH13.ThayDoiFragment
    TH13.ThayDoiFragment

---
+++ ReplacingFragmentsDynamically/app/src/main/java/yenly/edu/replacingfragmentsdynamically/FooterFragment.java
@@ -0,0 +1,65 @@
+package yenly.edu.replacingfragmentsdynamically;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class FooterFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_footer, container,false);
+        Button b1 =v.findViewById(R.id.button1);
+        Button b2 =v.findViewById(R.id.button2);
+        Button b3 =v.findViewById(R.id.button3);
+        FragmentManager fragmentManager=getParentFragmentManager();
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView_Content, new FragmentN1())
+                        .commit();
+
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView_Content, new FragmentN2())
+                        .commit();
+
+
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView_Content, new FragmentN3())
+                        .commit();
+
+
+            }
+        });
+
+        // Inflate the layout for this fragment
+        return v;
+    }
+}
---
+++ ReplacingFragmentsDynamically/app/src/main/java/yenly/edu/replacingfragmentsdynamically/FragmentN3.java
@@ -0,0 +1,35 @@
+package yenly.edu.replacingfragmentsdynamically;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FragmentN3#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FragmentN3 extends Fragment {
+
+    public FragmentN3() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_n3, container, false);
+    }
+}
