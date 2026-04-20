857b6ec033787f308e8b52b10fc1eed6a1b0111a
65131875-Nguyễn thị yến ly <ly.nty.65cntt@ntu.edu.vn>
2026-04-20 08:17:19 +0700

    commitlan2
    commitlan2

--- ThiGiuaKi2/app/src/main/java/thigk2/NguyenThiYenLy/DanhSachActivity.java
+++ ThiGiuaKi2/app/src/main/java/thigk2/NguyenThiYenLy/DanhSachActivity.java
@@ -19,16 +19,17 @@ public class DanhSachActivity extends AppCompatActivity {
         listView = findViewById(R.id.listViewCaKhuc);
         mangDuLieu = new ArrayList<>();
 
-        mangDuLieu.add(new ItemData(R.mipmap.ic_launcher, "1. Tiến quân ca", "Văn Cao"));
-        mangDuLieu.add(new ItemData(R.mipmap.ic_launcher, "2. Nguyễn Thị Yến Ly", "Ca khúc đặc biệt của sinh viên"));
-        mangDuLieu.add(new ItemData(R.mipmap.ic_launcher, "3. Tiến về Sài Gòn", "Lưu Hữu Phước"));
-        mangDuLieu.add(new ItemData(R.mipmap.ic_launcher, "4. Đất nước trọn niềm vui", "Hoàng Hà"));
-        mangDuLieu.add(new ItemData(R.mipmap.ic_launcher, "5. Bài ca thống nhất", "Võ Văn Di"));
-        mangDuLieu.add(new ItemData(R.mipmap.ic_launcher, "6. Mùa xuân trên Thành phố Hồ Chí Minh", "Xuân Hồng"));
-        mangDuLieu.add(new ItemData(R.mipmap.ic_launcher, "7. Chào anh giải phóng quân", "Hoàng Vân"));
-        mangDuLieu.add(new ItemData(R.mipmap.ic_launcher, "8. Dáng đứng Bến Tre", "Nguyễn Văn Tý"));
-        mangDuLieu.add(new ItemData(R.mipmap.ic_launcher, "9. Sợi nhớ sợi thương", "Phan Huỳnh Điểu"));
-        mangDuLieu.add(new ItemData(R.mipmap.ic_launcher, "10. Trường Sơn Đông, Trường Sơn Tây", "Hoàng Hiệp"));
+
+        mangDuLieu.add(new ItemData(R.drawable.tienquanca, "1. Tiến quân ca", "Văn Cao"));
+        mangDuLieu.add(new ItemData(R.drawable.anh1, "2. Nguyễn Thị Yến Ly", "Ca khúc đặc biệt của sinh viên"));
+        mangDuLieu.add(new ItemData(R.drawable.tienvesaigon, "3. Tiến về Sài Gòn", "Lưu Hữu Phước"));
+        mangDuLieu.add(new ItemData(R.drawable.datnuoctronniemvui, "4. Đất nước trọn niềm vui", "Hoàng Hà"));
+        mangDuLieu.add(new ItemData(R.drawable.baicathongnhat, "5. Bài ca thống nhất", "Võ Văn Di"));
+        mangDuLieu.add(new ItemData(R.drawable.muaxuanhcm, "6. Mùa xuân trên Thành phố Hồ Chí Minh", "Xuân Hồng"));
+        mangDuLieu.add(new ItemData(R.drawable.chaoanhgiaiphong, "7. Chào anh giải phóng quân", "Hoàng Vân"));
+        mangDuLieu.add(new ItemData(R.drawable.dangdungbentra, "8. Dáng đứng Bến Tre", "Nguyễn Văn Tý"));
+        mangDuLieu.add(new ItemData(R.drawable.soinhosoithuong, "9. Sợi nhớ sợi thương", "Phan Huỳnh Điểu"));
+        mangDuLieu.add(new ItemData(R.drawable.truongsondong, "10. Trường Sơn Đông, Trường Sơn Tây", "Hoàng Hiệp"));
 
         adapter = new MyAdapter(this, R.layout.item_list, mangDuLieu);
         listView.setAdapter(adapter);
