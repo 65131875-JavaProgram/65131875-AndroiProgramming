e98af8848e4971a02078f4e48e03e9876e6121d4
65131875-Nguyễn thị yến ly <ly.nty.65cntt@ntu.edu.vn>
2026-04-20 08:23:32 +0700

    commit lan 3
    commit lan 3

--- ThiGiuaKi2/app/src/main/java/thigk2/NguyenThiYenLy/trang3Activity.java
+++ ThiGiuaKi2/app/src/main/java/thigk2/NguyenThiYenLy/trang3Activity.java
@@ -1,24 +1,30 @@
 package thigk2.NguyenThiYenLy;
-
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import android.widget.ListView;
+import java.util.ArrayList;
 
 public class trang3Activity extends AppCompatActivity {
+
+    ListView listView;
+    ArrayList<ItemData> mangDuLieu;
+    MyAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_trang3);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        listView = findViewById(R.id.listViewDanhNhan);
+        mangDuLieu = new ArrayList<>();
+
+        mangDuLieu.add(new ItemData(R.drawable.hcm, "1. Chủ tịch Hồ Chí Minh", "Quê quán: Nam Đàn, Nghệ An"));
+        mangDuLieu.add(new ItemData(R.drawable.vonguyengiap, "2. Đại tướng Võ Nguyên Giáp", "Quê quán: Lệ Thủy, Quảng Bình"));
+        mangDuLieu.add(new ItemData(R.drawable.huongdaovuong, "3. Hưng Đạo Vương Trần Quốc Tuấn", "Quê quán: Tức Mặc, Nam Định"));
+        mangDuLieu.add(new ItemData(R.drawable.nguyentrai, "4. Nguyễn Trãi", "Quê quán: Thường Tín, Hà Nội"));
+        mangDuLieu.add(new ItemData(R.drawable.quangtrung, "5. Quang Trung - Nguyễn Huệ", "Quê quán: Tây Sơn, Bình Định"));
+
+        adapter = new MyAdapter(this, R.layout.item_list, mangDuLieu);
+        listView.setAdapter(adapter);
     }
 }
