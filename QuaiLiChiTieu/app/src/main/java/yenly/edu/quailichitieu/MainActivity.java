b8cc611e3a30569529143ea325bdaf55df2424e5
65131875-Nguyễn thị yến ly <ly.nty.65cntt@ntu.edu.vn>
2026-05-04 08:56:45 +0700

    GGfilebase
    GGfilebase

---
+++ QuaiLiChiTieu/app/src/main/java/yenly/edu/quailichitieu/MainActivity.java
@@ -0,0 +1,28 @@
+package yenly.edu.quailichitieu;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.Firebase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, Nguyen Thi Yen Ly");
+    }
+}
